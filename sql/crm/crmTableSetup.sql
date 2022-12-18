19c1c71c358c9a6f0e17748fb79af61c7f5f5764
RD <r.deans@outlook.com>
2022-12-18 09:59:15 -0500

    Organized information in crm, added sales territories:
    - user contact info is now in a seperate table
    - added tables for sales territories
    - added country, stateProvince tables

--- sql/crm/crmTableSetup.sql
+++ sql/crm/crmTableSetup.sql
@@ -1,4 +1,5 @@
 # refresh database
+drop table if exists clientContact;
 
 drop table if exists clientInfo;
 
@@ -16,7 +17,17 @@ drop table if exists client;
 
 drop table if exists customer;
 
+drop table if exists territoryStateProvince;
+
+drop table if exists stateProvince;
+
+drop table if exists country;
+
+drop table if exists territory;
+
 drop table if exists userAddress;
+
+drop table if exists userContact;
 
 drop table if exists userInfo;
 
@@ -25,6 +36,9 @@ drop table if exists timeZone;
 drop table if exists user;
 
 
+################################################
+# timezone
+################################################
 
 create table timeZone(
   id int not null auto_increment,
@@ -32,13 +46,38 @@ create table timeZone(
   primary key (id)
 );
 
+###################################################
+# Country
+###################################################
+
+create table country(
+  id int not null auto_increment,
+  fullName varchar(50) not null ,
+  abbreviation varchar(2) not null ,
+  primary key (id)
+);
 
 ##################################################
--- user
+# stateProvince
+##################################################
+create table stateProvince(
+   id int not null auto_increment,
+   countryId int not null ,
+   fullName varchar(50) not null ,
+   abbreviation varchar(2) not null ,
+   foreign key (countryId) references country(id),
+   primary key (id)
+);
+
+
+##################################################
+# user
 ##################################################
 create table user (
   id int not null auto_increment,
-  username varchar(45) not null,
+  username varchar(45) not null unique , # make sure admin
+                                        # and client dne with
+                                        # username
   password varchar(500) not null,
   primary key (id)
 );
@@ -46,23 +85,22 @@ create table user (
 -- bijection exists, fk can be used as pk
 create table userInfo (
     id int not null,
-    email varchar(45) not null,
     fName varchar(45) not null,
     lName varchar(45) not null,
-    phoneNumber int not null,
     timeZoneId int not null,
     primary key (id),
     foreign key (id) references user(id)
         on delete cascade,
     foreign key (timeZoneId) references timeZone(id)
 );
+
 -- require users to have an address, name, method of contact
 create table userAddress (
     id int not null,
     userId int not null,
-    state varchar(2) not null , #allow for abbreviated name only
+    stateProvince varchar(2) not null , #allow for abbreviated name only
     city varchar(45) not null ,
-    zip int not null ,
+    postalCode int not null ,
     streetName varchar(45) not null,
     streetNumber int not null,
     suite int,
@@ -71,6 +109,14 @@ create table userAddress (
         on delete cascade
 );
 
+create table userContact(
+  id int not null ,
+  phone int not null ,
+  altPhone int,
+  email varchar(45) not null,
+  foreign key (id) references user(id),
+  primary key (id)
+);
 
 #####################################################
 # Industry
@@ -88,7 +134,7 @@ create table industry(
 
 create table customer(
     id int not null auto_increment,
-    name varchar(50),
+    name varchar(50) ,
     primary key (id)
 );
 
@@ -121,7 +167,15 @@ create table clientInfo(
     fName varchar(45) not null ,
     lName varchar(45) not null ,
     phone int not null ,
-    timeZoneId int not null,
+    foreign key (id) references client(id),
+    primary key (id)
+);
+
+create table clientContact(
+    id int not null,
+    phone int not null,
+    email varchar(50) not null ,
+    timeZoneId int not null ,
     foreign key (id) references client(id),
     primary key (id),
     foreign key (timeZoneId) references timeZone(id)
@@ -145,6 +199,7 @@ create table salesOrder(
   id int not null auto_increment,
   salesPersonId int not null,
   clientId int not null,
+  salesTotal int not null,
   primary key (id),
   foreign key (salesPersonId) references user(id),
   foreign key (clientId) references client(id)
@@ -153,5 +208,29 @@ create table salesOrder(
 create table orderContent(
   id int not null,
   productId int not null , # retrieve from erp
+  qty int not null,
+  qtySaleValue int not null , # derived from erp
+  primary key (id),
   foreign key (id) references salesOrder(id)
+);
+
+########################################################
+# sales territories
+########################################################
+
+create table territory(
+  id int not null auto_increment,
+  salesPersonId int not null, # non-unique allows multiple
+                              #salespersons per territory
+  primary key (id),
+  foreign key (salesPersonId) references user(id)
+);
+
+create table territoryStateProvince(
+    id int not null auto_increment,
+    territoryId int not null ,
+    stateProvinceId int not null,
+    foreign key (territoryId) references territory(id),
+    foreign key (stateProvinceId) references stateProvince(id),
+    primary key (id)
 );
