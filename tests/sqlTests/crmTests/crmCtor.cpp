d1bf50514eefdc96d47a8fb2f0b86a092b8bd23e
RD <r.deans@outlook.com>
2022-12-21 11:53:53 -0500

    Forgot to implement test for crm ctor

--- tests/sqlTests/crmTests/crmCtor.cpp
+++ tests/sqlTests/crmTests/crmCtor.cpp
@@ -1,3 +1,11 @@
+#include <cassert>
+#include "../../../lib/sqlConnections/crmConnection.hpp"
 int main(){
+    try {
+        crmConnection test;
+    } catch (sqlException e) {
+        qWarning() << e.getMsg();
+        return 1;
+    }
     return 0;
 }
